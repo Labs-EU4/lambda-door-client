8b92a47e0410b68c3a0a70e5e6af8551b9c7395d
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-03-04 12:11:15 +0000

    getinterests working

--- src/state/actions/__test__/interests.test.js
+++ src/state/actions/__test__/interests.test.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import thunk from 'redux-thunk';
 import * as types from '../../types/index';
-import { getInterests } from '../interests';
+import { getUsersInterests, getInterests } from '../interests';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -29,5 +29,31 @@ describe('Action/types interests testing', () => {
 
     await store.dispatch(getInterests());
     expect(actions[0]).toEqual({ type: types.GET_INTERESTS });
+  });
+
+  it('should execute fetch interests data with success', async () => {
+    await mock
+      .onGet(`${process.env.REACT_APP_BACKEND_URL}/interests`)
+      .reply(200, interests);
+    const expectedActions = {
+      type: types.GET_INTERESTS_SUCCESS,
+      payload: interests,
+    };
+    const store = mockStore({});
+    const actions = store.getActions();
+    await store.dispatch(getInterests(1));
+    expect(actions[1]).toEqual(expectedActions);
+  });
+  it('should execute fetch error data', async () => {
+    const code = 401;
+    mock.onGet(`${process.env.REACT_APP_BACKEND_URL}/interests`).reply(code);
+    const expectedAction = {
+      type: types.GET_INTERESTS_FAILURE,
+      payload: `Request failed with status code ${code}`,
+    };
+    const store = mockStore({});
+    const actions = store.getActions();
+    await store.dispatch(getInterests(1));
+    expect(actions[1]).toEqual(expectedAction);
   });
 });
