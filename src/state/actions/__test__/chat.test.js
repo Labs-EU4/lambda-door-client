4a20916192d88126c282cb479e991eab2c3a0977
Evans Ibok <evansibok@gmail.com>
2020-03-17 17:55:35 +0100

    test(firestore): add chat.test.js file and add mock chats data

---
+++ src/state/actions/__test__/chat.test.js
@@ -0,0 +1,42 @@
+
+
+const chats = [
+  {
+    toUserID: 1,
+    fromUserName: "Rodrigo Graça",
+    fromUserID: 6,
+    toUserName: "Lisa Wilton",
+    open: true,
+    messages: [
+      {
+        fromUserID: 6,
+        message: 'this is a new message',
+        toUserID: 1,
+      },
+      {
+        fromUserID: 6,
+        message: 'this is a new message',
+        toUserID: 1,
+      },
+    ],
+  },
+  {
+    toUserID: 1,
+    fromUserName: "Rodrigo Graça",
+    fromUserID: 6,
+    toUserName: "Lisa Wilton",
+    open: true,
+    messages: [
+      {
+        fromUserID: 6,
+        message: 'second message',
+        toUserID: 1,
+      },
+      {
+        fromUserID: 6,
+        message: 'second message',
+        toUserID: 1,
+      },
+    ],
+  },
+]
